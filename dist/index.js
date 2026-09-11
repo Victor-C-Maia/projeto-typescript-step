abb7db4ccc19ab0245d5dd4bef06ee520d8eaea3
Victor-C-Maia <vcmaia.dev@gmail.com>
2026-09-10 21:37:58 -0300

    aula01 typescript

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let nome = "Mundo";
+console.log(`Olá, ${nome}!`);
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+let nome: string = "Mundo";
+
+console.log(`Olá, ${nome}!`);

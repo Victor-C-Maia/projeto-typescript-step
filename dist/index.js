b40916d96839512d2c5640d27eb668550062615a
Victor-C-Maia <vcmaia.dev@gmail.com>
2026-09-10 21:42:15 -0300

    aula 1 typescript introducao

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let nome = "Mundo";
 console.log(`Olá, ${nome}!`);
+const exemplo = {
+    nome: "Fabio",
+    saldo: 3000,
+};
+function deposito(saldoCliente, valorDeposito) {
+    return saldoCliente + valorDeposito;
+}
+console.log(deposito(exemplo.saldo, 152));
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
 let nome: string = "Mundo";
 
 console.log(`Olá, ${nome}!`);
+
+
+type Pessoa = {
+  nome: string;
+  saldo: number;
+};
+
+const exemplo: Pessoa = {
+  nome: "Fabio",
+  saldo: 3000,
+};
+
+function deposito(saldoCliente: number, valorDeposito: number): number {
+  return saldoCliente + valorDeposito;
+}
+
+console.log(deposito(exemplo.saldo, 152));
+          
